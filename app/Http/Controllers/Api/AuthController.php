23bf56062fb327dbf232c83d78fb843cfc9fde90
fennich-soufiane-fs <fennich.soufiane.fs@gmail.com>
2025-03-31 16:54:07 +0000

    Login & Logout APIs

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -26,5 +27,37 @@ class AuthController extends Controller
         return response()->json([
             'message' => "Account successfully created."
         ]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        if (!Auth::attempt($request->only('email', 'password')))
+        {
+            return response()->json([
+                'message' => 'Invalid Credentials',
+                'error_code' => 401
+            ], 401);
+        }
+        $user = Auth::user();
+        $user->tokens()->delete();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => "Login successfully.",
+            'user' => $user,
+            'access_token' => $token,
+        ]);
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+});
