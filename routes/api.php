6547bd96f8d49bb9e0050d9a0664a09ae4788390
fennich-soufiane-fs <fennich.soufiane.fs@gmail.com>
2025-03-31 15:27:14 +0000

    User Registration API

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::post('/register', [AuthController::class, 'register']);
-Route::get('', function () {
-    return response()->json(['message' => 'Test route working!']);
-});
